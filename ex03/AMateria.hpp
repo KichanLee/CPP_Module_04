823b2866bc2685447266f9b2993c472e4c51eccc
Kichan Lee <kichlee@c4r9s2.42seoul.kr>
2024-02-15 15:39:27 +0900

    test

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -1,12 +1,18 @@
 #ifndef __AMATERIA_HPP
 #define __AMATERIA_HPP
 
+class ICharacter;
+
 class AMateria {
- private:
+ protected:
   /* data */
  public:
   AMateria(/* args */);
+  AMateria(std::string const & type));
   ~AMateria();
+  std::string const& getType() const;
+  virtual AMateria* clone() const = 0;
+  virtual void use(ICharacter& target);
 };
 
 AMateria::AMateria(/* args */) {}
